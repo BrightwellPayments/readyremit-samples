1489643fa96e360d45c3bb19aed4fc6dd019f952
ferniluis <minafernando@gmail.com>
2022-09-10 23:58:22 +0200

    ReadyRemitModule for Android

--- readyremit_reactnative_demo/App.js
+++ readyremit_reactnative_demo/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -19,21 +19,36 @@ import {
 const { ReadyRemitModule } = NativeModules;
 
 const App = () => {
+  const eventEmitter = new NativeEventEmitter(ReadyRemitModule);
 
-const emitter = new NativeEventEmitter(ReadyRemitModule);
-emitter.addListener('auth', () => {
-  
-  // TODO: Fetch an auth token from your server
-  var authToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImlyUmg0cHJQSGxfdm5KSm15dVdrcyJ9.eyJodHRwczovL2FwaS5yZWFkeXJlbWl0LmNvbS9zZW5kZXJfaWQiOiJmMjY2ZDAzNi02OTVjLTQ2ODYtOTU4Yi01MTBjOGU1MGY4ZjAiLCJpc3MiOiJodHRwczovL3JlYWR5cmVtaXQudXMuYXV0aDAuY29tLyIsInN1YiI6InltWnlHc0xtQ21oejdHTVNRUTVPS1VWeFlydHVIZm90QGNsaWVudHMiLCJhdWQiOiJodHRwczovL3NhbmRib3gtYXBpLnJlYWR5cmVtaXQuY29tIiwiaWF0IjoxNjYyODI0MTAzLCJleHAiOjE2NjI5MTA1MDMsImF6cCI6InltWnlHc0xtQ21oejdHTVNRUTVPS1VWeFlydHVIZm90IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.sAwiTzEydnW8VDpolYp5AuoQbOXbnujrO8GEOCmyCR09uNYlqewBc8kXZRBzb8oVsYTiMumlkiY1DiKVbrN3ii33iZIUiqR2nQ-Y1RV_SuZc69FE-6PpaMc3Y3o8h0UlcGeGblm5wxJAdLnHKADp7X_vzsZJA82kd6jIk4Trf2DpQs2811kb5pPkVcwol9IPk1KpQcFAW07uF0O-gKN6GSZGKonoFNk0lHrbSj8KxSTq1uFFF1kvzb4b_KUNhQUjrfa5gyqi14CzyR0gU4xTeZllBoptl7NSFh86TiffjaXLcSeWQXubPm96QCTweFXvFoD8X0cnHlvOpy3L18q9Lw"
-  ReadyRemitModule.setAuthToken(authToken);
-});
+  useEffect(() => {
+    eventEmitter.addListener("READYREMIT_AUTH_TOKEN_REQUESTED", () => {
+      // TODO: Fetch an auth token from your server
+      ReadyRemitModule.setAuthToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImlyUmg0cHJQSGxfdm5KSm15dVdrcyJ9.eyJodHRwczovL2FwaS5yZWFkeXJlbWl0LmNvbS9zZW5kZXJfaWQiOiJjOTZhNzUyNi0yNDEyLTRlOGItYTZiMC1jZjcwYzNlNTVjNzIiLCJpc3MiOiJodHRwczovL3JlYWR5cmVtaXQudXMuYXV0aDAuY29tLyIsInN1YiI6InJyQmx6UlNQZzVVN3hqdkZmWngxOWlpZml1QjZQa3VjQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2Rldi1hcGkucmVhZHlyZW1pdC5jb20iLCJpYXQiOjE2NjI3Njg0OTgsImV4cCI6MTY2Mjg1NDg5OCwiYXpwIjoicnJCbHpSU1BnNVU3eGp2RmZaeDE5aWlmaXVCNlBrdWMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.fY9rAYnjEUcNIBKtEwUa5bybP7wD2mAWYNHNBrkmZWe5__gl4aWuV1A3SEX-IRXwJRFYAHRkUWdKqOXDjxnCn_vDM0C1aPOmMK3J388U7LKsJd6iNO5mWGd2HUC67wsreIb9T6tvUBHJSaYHqmdYlWUeO5bsW-7K6vMk7ClQwi2mLXsANl-HXzM5jDEfcCifVeBS1gMQXXmclWwCEdQEarDfcZ6GC4VaULVMmoV-BAVnjPXvSAnlUk1x86y1TrJHVUFuN3ltJkRurnA1NeOTacrWXNV6s7v4tU5oSFWii1FYgXLNM8ghGSVEGIa3xKh44lU1jzB48nfGX9gmvYRnxA", null);
+    })
+
+    return function cleanup () {
+      eventEmitter.removeAllListeners("READYREMIT_AUTH_TOKEN_REQUESTED");
+    }
+  }, []);
+
+  useEffect(() => {
+    eventEmitter.addListener("READYREMIT_TRANSFER_SUBMITTED", (request) => {
+      // TODO: Fetch a transferId from your server
+      ReadyRemitModule.setTransferId("f592edb9-f094-49cc-8f00-7c4168b80ed8", "");
+    })
+
+    return function cleanup () {
+      eventEmitter.removeAllListeners("READYREMIT_TRANSFER_SUBMITTED");
+    }
+  }, []);
 
 
   return (
     <SafeAreaView>
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Button title='Send Money' onPress={() => { ReadyRemitModule.launch(); }}></Button>
+        <Button title='Send Money' onPress={() => { ReadyRemitModule.launch("en"); }}></Button>
       </ScrollView>
     </SafeAreaView>
   );
