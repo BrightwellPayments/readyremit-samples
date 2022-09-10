495d4ff423bc36231011129cf8641d407a8379c4
ferniluis <minafernando@gmail.com>
2022-09-11 00:15:04 +0200

    Remove token and transfer id

--- readyremit_reactnative_demo/App.js
+++ readyremit_reactnative_demo/App.js
@@ -24,7 +24,7 @@ const App = () => {
   useEffect(() => {
     eventEmitter.addListener("READYREMIT_AUTH_TOKEN_REQUESTED", () => {
       // TODO: Fetch an auth token from your server
-      ReadyRemitModule.setAuthToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImlyUmg0cHJQSGxfdm5KSm15dVdrcyJ9.eyJodHRwczovL2FwaS5yZWFkeXJlbWl0LmNvbS9zZW5kZXJfaWQiOiJjOTZhNzUyNi0yNDEyLTRlOGItYTZiMC1jZjcwYzNlNTVjNzIiLCJpc3MiOiJodHRwczovL3JlYWR5cmVtaXQudXMuYXV0aDAuY29tLyIsInN1YiI6InJyQmx6UlNQZzVVN3hqdkZmWngxOWlpZml1QjZQa3VjQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2Rldi1hcGkucmVhZHlyZW1pdC5jb20iLCJpYXQiOjE2NjI3Njg0OTgsImV4cCI6MTY2Mjg1NDg5OCwiYXpwIjoicnJCbHpSU1BnNVU3eGp2RmZaeDE5aWlmaXVCNlBrdWMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.fY9rAYnjEUcNIBKtEwUa5bybP7wD2mAWYNHNBrkmZWe5__gl4aWuV1A3SEX-IRXwJRFYAHRkUWdKqOXDjxnCn_vDM0C1aPOmMK3J388U7LKsJd6iNO5mWGd2HUC67wsreIb9T6tvUBHJSaYHqmdYlWUeO5bsW-7K6vMk7ClQwi2mLXsANl-HXzM5jDEfcCifVeBS1gMQXXmclWwCEdQEarDfcZ6GC4VaULVMmoV-BAVnjPXvSAnlUk1x86y1TrJHVUFuN3ltJkRurnA1NeOTacrWXNV6s7v4tU5oSFWii1FYgXLNM8ghGSVEGIa3xKh44lU1jzB48nfGX9gmvYRnxA", null);
+      ReadyRemitModule.setAuthToken("", null);
     })
 
     return function cleanup () {
@@ -35,7 +35,7 @@ const App = () => {
   useEffect(() => {
     eventEmitter.addListener("READYREMIT_TRANSFER_SUBMITTED", (request) => {
       // TODO: Fetch a transferId from your server
-      ReadyRemitModule.setTransferId("f592edb9-f094-49cc-8f00-7c4168b80ed8", "");
+      ReadyRemitModule.setTransferId("", "");
     })
 
     return function cleanup () {
